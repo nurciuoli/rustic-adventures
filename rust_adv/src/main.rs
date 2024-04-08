213c1e2cf7c368ade5a3062bdd4d2de3bcb0d765
nurciuoli <naurciuoli@gmail.com>
2024-04-07 20:44:17 -0400

    first rust calc

--- rust_adv/src/main.rs
+++ rust_adv/src/main.rs
@@ -6,54 +6,65 @@ fn main() {
 
     let mut num_str =  String::new();
 
+    //get first input
     io::stdin()
         .read_line(&mut num_str)
         .expect("Failed to read line");
 
-
-    // Parse the string as a float
-    let num: f32 = num_str.trim().parse().expect("Please enter a valid number");
-    println!("You guessed: {num}");
-
-
-    // operator input
-    let mut operator = String::new();
-
-    // Loop until valid operator is entered
-    loop {
-        println!("Enter an operator (+, -, *, /):");
-        io::stdin().read_line(&mut operator).expect("Failed to read line");
-        operator = operator.trim().to_lowercase(); // Make operator case-insensitive
-
-        if ["+", "-", "*", "/"].contains(&&operator[..]) {
-            break; // Exit loop on valid operator
-        } else {
-            println!("Invalid operator. Please enter +, -, *, or /");
-            operator.clear(); // Clear invalid input for next attempt
-        }
-    }
-
-    // second number
-    let mut num2_str = String::new();
-
-    // Read second number
-    io::stdin().read_line(&mut num2_str).expect("Failed to read line");
-    let num2: f32 = num2_str.trim().parse().expect("Please enter a valid number");
-
-    // Perform calculation based on operator
-    let result = match operator.as_str() {
-        "+" => num + num2,
-        "-" => num - num2,
-        "*" => num * num2,
-        "/" => {
-            if num2 == 0.0 {
-                panic!("Division by zero is not allowed!");
-            } else {
-                num / num2
+    
+        // Parse the string as a float
+        let mut num: f32 = num_str.trim().parse().expect("Please enter a valid number");
+        //continue loop until bye
+        loop{
+            if num_str.trim().to_lowercase() == "bye" {
+                    println!("Bye-bye");
+                    break;
             }
-        },
-        _ => unreachable!(), // Shouldn't happen due to loop check
-    };
+            else{
 
-    println!("Result: {}", result);
-}
+            // operator input
+            let mut operator = String::new();
+
+                //get input for operator
+                println!("Enter an operator (+, -, *, /):");
+                io::stdin().read_line(&mut operator).expect("Failed to read line");
+                operator = operator.trim().to_lowercase(); // Make operator case-insensitive
+
+                if operator.to_lowercase() == "bye" {
+                    println!("Bye-bye");
+                    break;
+                }else{
+
+                if ["+", "-", "*", "/"].contains(&&operator[..]) {
+                    // second number
+                    let mut num2_str = String::new();
+
+                    // Read second number
+                    io::stdin().read_line(&mut num2_str).expect("Failed to read line");
+                    let num2: f32 = num2_str.trim().parse().expect("Please enter a valid number");
+
+                    // Perform calculation based on operator
+                    let result = match operator.as_str() {
+                        "+" => num + num2,
+                        "-" => num - num2,
+                        "*" => num * num2,
+                        "/" => {
+                            if num2 == 0.0 {
+                                panic!("Division by zero is not allowed!");
+                            } else {
+                                num / num2
+                            }
+                        },
+                        _ => unreachable!(), // Shouldn't happen due to loop check
+                    };
+
+                    println!("Result: {}", result);
+                    num = result;
+                } else {
+                    println!("Invalid operator. Please enter +, -, *, or /");
+                    operator.clear(); // Clear invalid input for next attempt
+                        }
+                    }    
+                }
+            }
+        }
